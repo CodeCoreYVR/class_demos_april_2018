cf1de8242ccfbde1c708311ce5507609d5733069
Steve Godin <steve.god@gmail.com>
2018-04-26 10:46:10 -0700

    Add ejs view engine and create home page

--- js_express_basics/fotorol/app.js
+++ js_express_basics/fotorol/app.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 
 const app = express();
 
+app.set("view engine", "ejs");
 app.use(logger("dev"));
 
 // URL http://localhost:454545/hello_world
@@ -31,6 +32,15 @@ app.get("/hello_world", (request, response) => {
   // the server's reply to the client. We use it to end a response
   // and send back data to the client.
   response.send("Hello, Thing!");
+});
+
+app.get("/", (request, response) => {
+  // `response.render` is used to render the contents of a
+  // template file located in the "views" directory.
+  // As a first argument, pass the path to the file
+  // beginning the "/views" directory without the file
+  // extension.
+  response.render("index");
 });
 
 const PORT = 4545;
