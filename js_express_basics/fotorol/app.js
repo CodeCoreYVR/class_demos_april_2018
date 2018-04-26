d44abebe83875377add6d0e2acdadba62bc3300a
Steve Godin <steve.god@gmail.com>
2018-04-26 10:17:17 -0700

    Add hello_world to express project

---
+++ js_express_basics/fotorol/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+// URL http://localhost:454545/hello_world
+// scheme   | host     | port| path
+
+// The "scheme" identifies the protocol being used.
+// Typically, HTTP or HTTPS, but could SSH, FTP, TCP, etc.
+
+// The "host" identify the domain or IP that holds the server.
+
+// The "path" identifes the specific resource we want to make
+// request to on the server.
+
+// This tells our app to respond to a GET request only
+// if the path is "/hello_world". When request is made to
+// that location by a client (i.e. browser), the callback
+// at argument 2 is called.
+
+// Inside the callback, we write code that builds the response.
+app.get("/hello_world", (request, response) => {
+  // The "request" argument is an object that represents
+  // the request being made by a client to the server. It
+  // contains all the information needed to respond back.
+
+  // The "response" argument is also an object that represents
+  // the server's reply to the client. We use it to end a response
+  // and send back data to the client.
+  response.send("Hello, World!");
+});
+
+const PORT = 4545;
+const DOMAIN = "localhost";
+app.listen(PORT, DOMAIN, () => {
+  console.log(`💻 Server listening on http://${DOMAIN}:${PORT}`);
+});
