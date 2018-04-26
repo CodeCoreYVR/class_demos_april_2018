f5eb2c605e8803dcdb4ff02f552d8635c5b89129
Steve Godin <steve.god@gmail.com>
2018-04-26 11:37:08 -0700

    Add contact_us page using query string

--- js_express_basics/fotorol/app.js
+++ js_express_basics/fotorol/app.js
@@ -6,8 +6,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(logger("dev"));
 
-// URL http://localhost:454545/hello_world
-// scheme   | host     | port| path
+// URL http://localhost:454545/hello_world?name=steve&message=hello+there
+// scheme   | host     | port| path       | query string
 
 // The "scheme" identifies the protocol being used.
 // Typically, HTTP or HTTPS, but could SSH, FTP, TCP, etc.
@@ -16,6 +16,13 @@ app.use(logger("dev"));
 
 // The "path" identifes the specific resource we want to make
 // request to on the server.
+
+// The "query string" is way to store data in a URL.
+// It uses the "url encoding" format.
+// A query sting always begins with "?".
+// It is then followed by key-values pairs.
+// A key value pair defined as such {key}={value}
+// Pairs are seperated by "&".
 
 // This tells our app to respond to a GET request only
 // if the path is "/hello_world". When request is made to
@@ -41,6 +48,21 @@ app.get("/", (request, response) => {
   // beginning the "/views" directory without the file
   // extension.
   response.render("index");
+});
+
+// HTTP VERB: GET, URL: http://localhost:4545/contact_us
+app.get("/contact_us", (request, response) => {
+  console.log(request.query);
+
+  // To access data in a query string of a url,
+  // use the property "query" of request. "request.query"
+  // will have a JavaScript that's been parsed
+  // from the query string.
+
+  const name = request.query.name;
+  const message = request.query.message;
+
+  response.render("contact_us", { name: name, message: message });
 });
 
 const PORT = 4545;
