61056b07e0fbf239ccde77306d5381c850faacde
Steve Godin <steve.god@gmail.com>
2018-06-26 13:52:04 -0700

    Add demo for Change the Output

--- js_webpack_basics/webpack.config.js
+++ js_webpack_basics/webpack.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 // When writing a Webpack config, we must use
 // Node's module style (i.e. require and module.exports)
 // because this file is not processed by Webpack.
@@ -8,9 +10,21 @@ module.exports = {
   // There can be multiple per project.
   // entry: ["./src/index.js", "./src/index_2.js"]
   // When using an object keys will act as names
-  // and will be used the name of the output files
+  // and will be used as the name of the output files
   // by default.
   entry: {
     app: "./src/app.js"
+  },
+  // The following can be used to configure the output files
+  output: {
+    // `path` specify the directory where processed files
+    // are saved.
+    path: path.join(__dirname, "dist"),
+    // `filename` can be used to specify the name of
+    // your bundled files.
+    // filename: "bundle.js"
+    // You can use [name] in the `filename` to interpolate
+    // the name of entry file `app` in this case.
+    filename: "[name]_bundle.js"
   }
 };
