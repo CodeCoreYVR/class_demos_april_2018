035f56cf73eb1f7813ea351e1d7376dcd1df2d00
Steve Godin <steve.god@gmail.com>
2018-06-26 13:45:05 -0700

    Add demo for Change the Entry

--- js_webpack_basics/webpack.config.js
+++ js_webpack_basics/webpack.config.js
@@ -2,5 +2,15 @@
 // Node's module style (i.e. require and module.exports)
 // because this file is not processed by Webpack.
 module.exports = {
-  mode: "development"
+  mode: "development",
+  // You can specify entry in multiples ways.
+  // entry: "./src/index.js"
+  // There can be multiple per project.
+  // entry: ["./src/index.js", "./src/index_2.js"]
+  // When using an object keys will act as names
+  // and will be used the name of the output files
+  // by default.
+  entry: {
+    app: "./src/app.js"
+  }
 };
